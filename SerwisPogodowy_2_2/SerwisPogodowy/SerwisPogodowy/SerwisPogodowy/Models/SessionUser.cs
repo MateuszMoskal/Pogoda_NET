9eba8912fd77c6c2fde23ca23e36fc65153330ba
MateuszMoskal <mateusz.moskal07@wp.pl>
2025-04-25 12:57:51 +0200

    Mój błąd
    Dodałem drugi folder przez przypadek

--- SerwisPogodowy_2_2/SerwisPogodowy/SerwisPogodowy/SerwisPogodowy/Models/SessionUser.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SerwisPogodowy.Models
-{
-    public class SessionUser
-    {
-        public string Email { get; set; } = string.Empty;
-        public int Id { get; set; }
-    }
-}
--- SerwisPogodowy_2_2/SerwisPogodowy/SerwisPogodowy/SerwisPogodowy/Models/ViewModels/CitySearchVM.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SerwisPogodowy.Models.ViewModels
-{
-    public class CitySearchVM
-    {
-        public string CityName { get; set; } = string.Empty;
-
-        public List<City>? Cities { get; set; } = null;
-    }
-}
